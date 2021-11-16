0692c7c0fb462c78d05de7de1dfd0f2fda40c28d
DESKTOP-G3UMIE3\huuta <huutam.ptnk@gmail.com>
2021-11-16 08:23:23 +0200

    Update schema for task.

---
+++ src/model/task.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    default: "unknown",
+    required: true,
+  },
+  description: {
+    type: String,
+    default: "unknown"
+  },
+  priority: {
+    type: String,
+    default: "0",
+    required: true,
+  },
+  status: {
+    type: String,
+    default: "0",
+    required: true,
+  },
+  due: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  }
+});
+
+module.exports = mongoose.model('Task', taskSchema);
