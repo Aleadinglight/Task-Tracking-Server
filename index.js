ee747852a5dfd0595ea6c9c140a7515bffb96e66
DESKTOP-NFJU7D6\Tam <huutamnguyen2302@gmail.com>
2022-01-29 04:08:53 +0200

    Fix lint error in index.js

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-const express = require('express')
-var cors = require('cors')
-const app = express()
-const database = require('./src/database/myMongoose')
-const taskRoute = require('./src/route/taskRoute')
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const database = require('./src/database/myMongoose');
+const taskRoute = require('./src/route/taskRoute');
+require('dotenv').config();
 
-// Import database 
+// Import database
 database();
 
 // Configure cors options
-let corsOptions = {
+const corsOptions = {
   origin: 'http://localhost:3000',
-  optionsSuccessStatus: 200
-}
+  optionsSuccessStatus: 200,
+};
 
 app.use(cors(corsOptions));
 app.use(express.json());
-app.set('json spaces', 2)
-app.get("/", (req, res) => {
-  res.send('Hello World!')
+app.set('json spaces', 2);
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
 app.use('/tasks', taskRoute);
 
 const port = Number(process.env.PORT) || 3001;
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
