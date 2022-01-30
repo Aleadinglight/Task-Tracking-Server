164f6e21c6d8cff8ac6e57bb028a83cf92e055ea
DESKTOP-NFJU7D6\Tam <huutamnguyen2302@gmail.com>
2022-01-30 13:34:48 +0200

    Refactor index.js with new port environment variable.

--- index.js
+++ index.js
@@ -3,22 +3,27 @@ const cors = require('cors');
 const database = require('./src/database/myMongoose');
 const taskRoute = require('./src/route/taskRoute');
 const corsOptions = require('./src/config/corsOption');
+const { port } = require('./src/config/vars');
 
 // Establish the database connection
 database.connect();
 
 const app = express();
+
+// Enable CORS
 app.use(cors(corsOptions));
+
+// Use express json parse
 app.use(express.json());
 app.set('json spaces', 2);
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 app.use('/tasks', taskRoute);
 
-const port = Number(process.env.PORT) || 3001;
-
+// Start the server
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
