41e2a7db90adba5a80455150c85aed2ccc8acad6
DESKTOP-G3UMIE3\huuta <huutam.ptnk@gmail.com>
2022-02-19 23:07:52 +0200

    Add database test

---
+++ test/unit/database.test.js
@@ -0,0 +1,22 @@
+const request = require('supertest');
+const app = require('../../src/config/app');
+const database = require('../../src/database/myMongoose');
+
+describe('Test database open and close correctly', () => {
+  beforeAll(async () => {
+    connection = await database.connect();
+
+    // Connection should not be null or undefined
+    expect(connection).not.toBeNull();
+    expect(connection).not.toBeUndefined();
+  });
+
+  afterAll(async () => {
+    await database.disconnect();
+  });
+
+  test('Test server work with database', async () => {
+    const response = await request(app).get('/tasks');
+    expect(response.statusCode).toBe(200);
+  });
+});
